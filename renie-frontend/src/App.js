75e1bff50bedb4aabfd7dd3904d4692db49b44f8
samarjit <samarjitbravo@gmail.com>
2024-06-14 21:29:14 +0530

    Set up frontend

---
+++ renie-frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-red-300">
+        <p className="bg-green-300 underline text-sm rotate-90">Hello World</p>
+        <p className="bg-orange-300 w-96">Learn React</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
