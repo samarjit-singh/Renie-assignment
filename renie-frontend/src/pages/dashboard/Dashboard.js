f85783fa5038f29dd6c79d043bae52e6eac7eba6
samarjit <samarjitbravo@gmail.com>
2024-06-15 13:32:45 +0530

    Create Sidebar

--- renie-frontend/src/pages/dashboard/Dashboard.js
+++ renie-frontend/src/pages/dashboard/Dashboard.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Navigation from "./_components/navigation";
+import { BiSolidFoodMenu } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isSmallScreen = window.innerWidth <= 1023;
+      setSidebarOpen(!isSmallScreen);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div className="h-screen flex relative">
+      <Navigation
+        isSidebarOpen={isSidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+      />
+      <main className="flex-1 relative h-full overflow-y-auto">
+        <BiSolidFoodMenu
+          className="h-6 w-6 cursor-pointer absolute top-5 left-1 lg:hidden"
+          onClick={toggleSidebar}
+        />
+        <p>Hello World</p>
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ renie-frontend/src/pages/dashboard/_components/navigation.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FaAngleDoubleLeft } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
+const navigation = ({ isSidebarOpen, setSidebarOpen }) => {
+  const handleToggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <aside
+        className={`group/sidebar font-mono h-full border-r-4 border-dashed border-r-[#3D3B40] overflow-y-auto relative flex ${
+          isSidebarOpen ? "w-full lg:w-80" : "w-0"
+        } flex-col z-20 transition-width ease-in-out duration-300`}
+      >
+        <div className="flex items-center justify-between hover:bg-primary/5">
+          <div
+            role="button"
+            className="flex items-center text-sm p-3 w-full hover:bg-gray-100"
+          >
+            <div className="gap-x-2 flex items-center max-w-[200px]">
+              <CgProfile className="h-5 w-5 ml-4" />
+
+              <span className="text-start font-bold">
+                Samarjit@bennett.in
+              </span>
+            </div>
+          </div>
+
+          <p onClick={handleToggleSidebar} className="lg:hidden">
+            <FaAngleDoubleLeft className="h-5 w-5 mr-6 " />
+          </p>
+        </div>
+
+        <Link to="/dashboard/recycle">
+          <div className="flex items-center justify-between h-8 hover:bg-gray-100 cursor-pointer">
+            <span className="flex gap-x-3">
+              <p className="h-5 w-5 ml-4">♻️</p>
+              <p className="font-bold">Recycle</p>
+            </span>
+          </div>
+        </Link>
+        <div className="flex items-center justify-between h-8 hover:bg-gray-100 cursor-pointer">
+          <span className="flex gap-x-3">
+            <p className="h-5 w-5 ml-4">📱</p>
+            <p className="font-bold">Username Dashboard</p>
+          </span>
+        </div>
+        <div className="flex items-center justify-between h-8 hover:bg-gray-100 cursor-pointer">
+          <span className="flex gap-x-3">
+            <p className="h-5 w-5 ml-4">🧑🏽‍💻</p>
+            <p className="font-bold">Public Dashboard</p>
+          </span>
+        </div>
+        <div className="flex items-center justify-between h-8 hover:bg-gray-100 cursor-pointer">
+          <span className="flex gap-x-3">
+            <p className="h-5 w-5 ml-4">⚙️</p>
+            <p className="font-bold">Settings</p>
+          </span>
+        </div>
+      </aside>
+    </>
+  );
+};
+
+export default navigation;
