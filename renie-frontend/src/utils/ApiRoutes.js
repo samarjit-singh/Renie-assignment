5ef0d02c5400f5733c77542112ba3495b5ac52aa
samarjit <samarjitbravo@gmail.com>
2024-06-17 16:15:39 +0530

    host update

--- renie-frontend/src/utils/ApiRoutes.js
+++ renie-frontend/src/utils/ApiRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+export const host = "https://renie-assignment.onrender.com";
 
 export const userRoute = `${host}/user`;
 export const transactionRoute = `${host}/transaction`;
